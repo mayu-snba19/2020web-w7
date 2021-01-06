a77c661174ea3d92c506a0e0c5b9a146524054af
kishi4web <75372859+kishi4web@users.noreply.github.com>
2021-01-07 02:36:23 +0900

    Create shared.js

---
+++ shared.js
@@ -0,0 +1,8 @@
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+setCookie("class","web technology b", 30);
